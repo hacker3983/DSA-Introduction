625c662676f849cdcaf75e0879f2b8cf98e83951
hacker3983 <mrgamer19554@gmail.com>
2024-09-23 17:44:15 -0500

    Implemented singly linked list

---
+++ singly-linkedlist.h
@@ -0,0 +1,17 @@
+#ifndef _SINGLY_LINKEDLIST_H
+#define _SINGLY_LINKEDLIST_H
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct singly_linkedlist {
+	int value;
+	struct singly_linkedlist *next, *tail;
+} singly_llist;
+
+singly_llist* singly_linkedlist_create(int value);
+void singly_linkedlist_insertfront(singly_llist** singly_linkedlist, int value);
+void singly_linkedlist_append(singly_llist** singly_linkedlist, int value);
+void singly_linkedlist_remove(singly_llist** singly_linkedlist, int value);
+void singly_linkedlist_print(singly_llist* singly_linkedlist);
+void singly_linkedlist_destroy(singly_llist** singly_linkedlist);
+#endif
